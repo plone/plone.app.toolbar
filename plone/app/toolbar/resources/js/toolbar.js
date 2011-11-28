dbdb2a4b7b50eb2196fe4b5c4578959d6b86e7ff
Rok Garbas <rok@garbas.si>
2011-11-28 13:28:36 +0100

    preparing for integration of "new" toolbar.js lib
     - add "new" toolbar.js library with dependency
     - removing not needed bootstrap js libraries
     - removing styles that are not needed

--- plone/app/toolbar/resources/js/toolbar.js
+++ plone/app/toolbar/resources/js/toolbar.js
@@ -1,157 +1,320 @@
-/*
- */
+// Version: 0.1 (unrelised)  
+// (c) 2011 Rok Garbas  
+// May be freely distributed under the BSD license.
+//
+// For all details and documentation:  
+// <http://garbas.github.com/jquery.toolbar.js>
 
 (function ($) {
 
- /*jslint white:false, onevar:true, undef:true, nomen:false, eqeqeq:true,
-   plusplus:true, bitwise:true, regexp:false, newcap:true, immed:true,
-   strict:false, browser:true */
-/*global jQuery:false, $:false, document:false, window:false, location:false,
-  common_content_filter:false, TinyMCEConfig:false pb:false */
-    var default_config = {
-        actions: [{
-            bind_to: '.dropdownLink',
-            is_open: function(toolbar) {
-                if ($('.dropdownItems:visible').length === 0) {
-                    return false;
-                } else {
-                    return true;
-                }
-            }
-        }]
+    // # Namespace
+    $.toolbar = {};
+
+    // # Default options
+    $.toolbar.options = $.toolbar.default_options = {
+        id: 'jquery-toolbar',
+        name: 'jquery-toolbar',
+        klass: 'toolbar',
+        klass_category_prefix: 'toolbar-category-',
+        buttons: []
     };
 
-    function Toolbar(config, toolbar) {
+    // # Router
+    var Router = Backbone.Router.extend({
 
-        var toolbar_stretched = false;
-        var toolbar_height = toolbar.outerHeight();
-        var content_height = $(window.parent.document).height();
+        initialize: function(options) {
 
-        //TODO: need to handle scroll offset:
-        //      actually check if its being handled properly since its
-        //      working in FF9
+            $.toolbar.iframe = new $.toolbar.view.IFrame(options);
 
-        //TODO: need to handle ovelays:
-        //      not sure if this is part of "deco light" but functionelity
-        //      should definetly be ported at some point
-
-        function stretch() {
-            //toolbar_offset = $(window.parent).scrollTop();
-            //$('body', window.parent.document).css('overflow', 'hidden');
-            //$(window.parent).scrollTop(toolbar_offset);
-            toolbar.height(content_height);
-            toolbar_stretched = true;
         }
 
-
-        function shrink() {
-            //$('body', window.parent.document).css('overflow', 'auto');
-            //$(window.parent).scrollTop(toolbar_offset);
-            toolbar.height(toolbar_height);
-            toolbar_stretched = false;
-        }
-
-        // is somebody clicks on streched iframe then we shrink it
-        $(window).bind('click', function(e) {
-            if (toolbar_stretched === true) {
-                shrink();
-            }
-        });
-
-
-        config['actions'].every( function(item) {
-            $(item.bind_to).bind('click', function(e) {
-                if (toolbar_stretched === false) {
-                    stretch();
-                } else {
-                    shrink();
-                }
-                e.preventDefault();
-            });
-        });
-
-        return {
-            config: config,
-            default_config: default_config,
-            shrink: shrink,
-            stretch: stretch
-        };
-    }
-
-    $.fn.toolbar = function (custom_config) {
-
-        // is plugin already installed
-        if (this.data('toolbar')) {
-            return this.data('toolbar');
-        }
-
-        // TODO: only instantiate if this if element we are refering to is iframe
-
-        // "compile" configuration for widget
-        var config = $.extend({}, default_config);
-        $.extend(config, custom_config);
-
-        //
-        var toolbar = new Toolbar(config, $(this));
-        this.data('toolbar', toolbar);
-        return toolbar;
-
-    };
-
-}(jQuery));
-
-
-/* 
- * Initialization of plone's toolbar
- */
-$(document).ready(function () {
-    $('#plone-toolbar', window.parent.document).toolbar({
-        actions: [{
-            bind_to: 'dt.actionMenuHeader a',
-            is_open: function() {
-                if ($('dd.actionMenuContent:visible').length === 0) {
-                    return false;
-                } else {
-                    return true;
-                }
-            }
-        }]
     });
-    /******
-        override p.a.jquerytool's create_content_div method (in overlayhelpers.js)
-        to create a bootstrap compatible overlay container
-    ******/
-    pb.create_content_div = function (pbo, trigger) {
-        var content,
-            top,
-            pbw = pbo.width;
 
-        content = $(
-            '<div id="' + pbo.nt +
-            '" class="modal overlay-' + pbo.subtype +
-            ' ' + (pbo.cssclass || '') +
-            '"><div class="modal-header"><div class="close"><span>Close</span></div><h3>XXX</h3></div><div class="modal-body"></div></div>'
-        );
 
-        content.data('pbo', pbo);
 
-        // if a width option is specified, set it on the overlay div,
-        // computing against the window width if a % was specified.
-        if (pbw) {
-            if (pbw.indexOf('%') > 0) {
-                content.width(parseInt(pbw, 10) / 100 * $(window).width());
-            } else {
-                content.width(pbw);
+    // # Models
+    $.toolbar.models = {};
+
+    // ## Button
+    $.toolbar.models.Button = Backbone.Model.extend({
+        initialize: function() {
+            if (this.get('category') === undefined) {
+                this.set({ category: 'default' });
+            }
+        }
+    });
+
+
+
+    // # Collections
+    $.toolbar.collection = {};
+
+    // ## Buttons
+    $.toolbar.collection.Buttons = Backbone.Collection.extend({
+        model: $.toolbar.models.Button
+    });
+    $.toolbar.buttons = new $.toolbar.collection.Buttons();
+
+
+
+    // # Views
+    $.toolbar.view = {};
+
+    // ## IFrame
+    $.toolbar.view.IFrame= Backbone.View.extend({
+        tagName: 'iframe',
+        attributes: { allowtransparency: 'true' },
+
+        initialize: function() {
+            this.render();
+        },
+
+        render: function() {
+
+            $('body').prepend(this.el);
+
+            var buttons = new $.toolbar.view.Buttons({
+                className: 'toolbar',
+                collection: $.toolbar.buttons
+            });
+
+            // Add buttons to toolbar
+            _.forEach($.toolbar.options.buttons, function(item) {
+                $.toolbar.buttons.add(item);
+            });
+
+            $(this.el).load(function() {
+
+                var toolbar_wrapper = $('<div/>', {'class': 'toolbar-wrapper'});
+                toolbar_wrapper.append(buttons.el);
+
+                $('body', $($.toolbar.iframe.el).contents()).prepend(toolbar_wrapper);
+
+                $.toolbar.iframe.height = $($.toolbar.iframe.el).height();
+
+                $('a', $($.toolbar.iframe.el).contents()).bind('click', {
+                    window: window
+                }, function(event) {
+
+                    var url = $(event.target).attr('href');
+
+                    if (url !== '#') {
+
+                        window.location = url;
+
+                    }
+                });
+
+            });
+
+            return this;
+        },
+
+        make: function(tagName, attributes, content) {
+            _.extend(attributes, {
+                'id':     this.options.id,
+                'src':    this.options.src,
+                'class':  this.options.klass,
+                'name':  this.options.name
+            });
+            var el = document.createElement(tagName);
+            if (attributes) $(el).attr(attributes);
+            return el;
+
+        }
+
+    });
+
+    // ## Buttons
+    $.toolbar.view.Buttons = Backbone.View.extend({
+
+        initialize: function(options) {
+            options.collection.bind('add', this.renderItem, this);
+        },
+
+        renderItem: function(model) {
+            var button = new $.toolbar.view.Button({model: model});
+
+            var category_name = $.toolbar.options.klass_category_prefix +
+                model.get('category');
+
+            var category = this.$('.'+category_name);
+            if (category.length === 0)  {
+                category = $('<div/>', {'class': category_name});
+                category.html('<ul></ul>');
+                $(this.el).append(category);
+            }
+
+            this.$('div.' + category_name +' > ul').append(button.el);
+        }
+
+    });
+
+    // ## Button
+    $.toolbar.view.Button = Backbone.View.extend({
+        tagName: 'li',
+
+        initialize: function() {
+            this.render();
+        },
+
+        render: function() {
+
+            var options = _.extend({
+                id: '',
+                url: '#',
+                klass: 'button',
+                submenu: [],
+                events: [],
+                initialize: function() {}
+            }, this.model.attributes);
+
+            this.renderButton($(this.el), options);
+
+        },
+
+        renderButton: function(el, options) {
+
+            var button = $('<a/>', {
+                'id': options.id,
+                'class': options.klass,
+                'href': $.toolbar.utils.url_join(window.location.href, options.url)
+            });
+            button.html(options.title);
+            el.html(button);
+
+            options.initialize(this);
+
+            if (options.submenu.length > 0) {
+
+                var collection = new $.toolbar.collection.Buttons();
+
+                this.submenu = new $.toolbar.view.Buttons({
+                    className: 'toolbar-submenu',
+                    collection: collection
+                });
+                el.append(this.submenu.el); 
+
+                $(this.submenu.el).hide();
+
+                button.bind('click', { button: this }, function(event) {
+                    $(event.data.button.el).addClass('active');
+                    $(event.data.button.submenu.el).show();
+                    $($.toolbar.iframe.el).height($(window.document).height());
+
+                    event.stopPropagation();
+                    $($.toolbar.iframe.el).contents().bind('click', {
+                        button: event.data.button
+                    }, function(event) {
+                        $(event.data.button.el).removeClass('active');
+                        $(event.data.button.submenu.el).hide();
+                        $($.toolbar.iframe.el).height($.toolbar.iframe.height);
+                    });
+                });
+
+                // bellow is "hover" version of button
+                //button.bind('mouseenter', { button: this }, function(event) {
+                //    $(event.data.button.el).addClass('active');
+                //    $(event.data.button.submenu.el).show();
+                //    $($.toolbar.iframe.el).height($(window.document).height());
+                //});
+                //$(this.el).bind('mouseleave', { button: this
+                //}, function(event) {
+                //    $(event.data.button.el).removeClass('active');
+                //    $(event.data.button.submenu.el).hide();
+                //    $($.toolbar.iframe.el).height($.toolbar.iframe.height);
+                //});
+
+                _.forEach(options.submenu, function(item) {
+                    this.submenu.collection.add(item);
+                }, this);
+
+            }
+
+            button.bind(options.events);
+
+        }
+
+    });
+
+    
+    // # Utils
+    $.toolbar.utils = {};
+
+    // ## Absolute URL
+    //
+    // TODO: needs tests
+    // taken from http://stackoverflow.com/q/8014921
+    $.toolbar.utils.url_join = function(base, relative) {
+
+        // return "empty" links
+        if (relative === '#') {
+            return relative;
+        }
+
+        // See if there is already a protocol on this
+        if (relative.indexOf("://") != -1) {
+            return relative;
+        }
+
+        // See if this is protocol-relative
+        if (relative.indexOf("//") === 0) {
+            var protocolIndex = base.indexOf("://");
+            return base.substr(0, protocolIndex+1) + relative;
+        }
+
+        // We need to split the domain and the path for the remaining options
+        var protocolIndexEnd = base.indexOf("://") + 3;
+
+        // append slash if passed only http://bla.com
+        if (base.indexOf("/", protocolIndexEnd) == -1) {
+            base += "/";
+        }
+
+        var endDomainIndex = base.indexOf("/", protocolIndexEnd);
+        var domain = base.substr(0, endDomainIndex);
+        var path = base.substr(endDomainIndex);
+
+        // trim off any ending file name
+        if (path.lastIndexOf("/") != path.length-1) {
+            path = path.substr(0, path.lastIndexOf("/")+1);
+        }
+
+        // See if this is site-absolute
+        if (relative.indexOf("/") === 0) {
+            return domain + relative;
+        }
+
+        // See if this is document-relative with ../
+        while (relative.indexOf("../") === 0) {
+            relative = relative.substr(3);
+            if (path.length > 1) {
+                var secondToLastSlashIndex = path.substr(0, path.length-1).lastIndexOf("/");
+                path = path.substr(0, secondToLastSlashIndex+1);
             }
         }
 
-        // add the target element at the end of the body.
-        if (trigger) {
-            trigger.after(content);
-        } else {
-            content.appendTo($("body"));
-        }
-
-        return content;
+        // Finally, slap on whatever ending is left
+        return domain + path + relative;
     };
-});
+
+    // # Initialization method
+    $.toolbar.initialize = function(custom_options) {
+
+        $.toolbar.options = _.extend($.toolbar.default_options, custom_options);
+
+        $(document).ready(function() {
+
+            $.toolbar.router = new Router($.toolbar.options);
+
+        });
+    };
+
+    $.toolbar.remove = function() {
+        $.toolbar.options = $.toolbar.default_options;
+        $.toolbar.iframe.remove();
+        $.toolbar.buttons = new $.toolbar.collection.Buttons();
+        console.log('reseting toolbar');
+    };
+
+})(jQuery);
