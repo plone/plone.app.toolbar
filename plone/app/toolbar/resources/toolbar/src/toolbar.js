4e9639e8925a2f1574a63af47d4f6b821d52506c
Ross Patterson <me@rpatterson.net>
2012-03-08 03:02:42 -0800

    Overlays are back

--- plone/app/toolbar/resources/toolbar/src/toolbar.js
+++ plone/app/toolbar/resources/toolbar/src/toolbar.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    // Shrink the iframe back down after closing a menu
     $(document).mousedown(function (event) {
         if (jQuery(event.target).parents('.actionMenu:first').length) {
             // target is part of the menu, so just return and do the default
@@ -13,4 +14,81 @@ $(document).ready(function() {
         $('#plone-toolbar', parent_doc).height(
             $(parent_doc).height());
     });
+
+    // # overlay {{{
+    function overlay(e) {
+        var headers = {},
+            selector = "#portal-column-content",
+            el = $(e.target),
+            href = el.closest('a').attr('href'),
+            parent_doc = window.parent.document;
+            modal = $('#toolbar-overlay', parent_doc),
+            body = $('.modal-body', modal),
+            rscript = /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi;
+
+        // Clean up the url
+        href = (href.match(/^([^#]+)/)||[])[1];
+        headers['X-Theme-Disabled'] = "True";
+        $.ajax({
+            url: href,
+            headers: headers,
+            complete: function(jqXHR, status, response) {
+                // Store the response as specified by the jqXHR object
+                response = jqXHR.responseText;
+                // If successful, inject the HTML into all the matched elements
+                if (jqXHR.isResolved()) {
+                    jqXHR.done(function( r ) {
+                        response = r;
+                    });
+                    body.empty().html(jQuery("<div>").append(
+                        response.replace(rscript, "")).find(
+                        selector));
+                }
+
+                function setupOverlay(){
+                    // Keep all links inside the overlay
+                    $('a', body).on('click', overlay);
+
+                    // Keep all forms inside the overlay
+                    $('form', body).ajaxForm({
+                        target: body,
+                        success: setupOverlay
+                    });
+
+                    $(document).trigger('setupOverlay',
+                        [modal, response, status]);
+                }
+                setupOverlay();
+                modal.modal('show');
+            }
+        });
+        e.preventDefault();
+    }
+    // }}}
+
+    // capture all clicks on iframe
+    $(document).bind('click', {
+        self: self, document: document
+    }, function(e) {
+        if (e.which === 1) {
+            var self = e.data.self,
+                el = $(e.target);
+
+            if (!$.nodeName(e.target, 'a')) {
+                el = el.closest('a');
+            }
+
+            // Buttons default to an overlay but if they
+            // have the '_parent' link target, just load them in
+            // the window
+            if (el.attr('target') == '_parent') {
+                return true;
+            } else {
+                overlay(e);
+            }
+
+            return e.preventDefault();
+        }
+    });
+
 });
