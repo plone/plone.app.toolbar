8265389f34cf4115a29b33de84440268f7dee71c
Izak Burger <isburger@gmail.com>
2012-03-09 23:29:26 +0200

    simplify, remove setupOverlay function that served no purpose anymore.

--- plone/app/toolbar/resources/toolbar/src/toolbar.js
+++ plone/app/toolbar/resources/toolbar/src/toolbar.js
@@ -47,35 +47,34 @@ $(document).ready(function() {
 
         body.empty().load(href + ' #portal-column-content',
             function(response, error){
-                function setupOverlay(){
-                    // Keep all links inside the overlay
-                    $('a', body).on('click', overlay);
+                // Keep all links inside the overlay
+                $('a', body).on('click', overlay);
 
-                    // Init plone forms if they exist
-                    if ($.fn.ploneTabInit) {
-                        body.ploneTabInit();
-                    }
-
-                    // Tinymce editable areas inside overlay
-                    $('textarea.mce_editable', body).each(function() {
-                        var id = $(this).attr('id'),
-                            config = new TinyMCEConfig(id);
-                        // Forgive me for I am about to sin. But it does mean
-                        // we can overlay it multiple times. If you know a
-                        // better way, please share.
-                        delete InitializedTinyMCEInstances[id];
-                        config.init();
-                    });
-
-                    // Call any other event handlers
-                    $(document).trigger('setupOverlay',
-                        [modal, response, error]);
-
+                // Init plone forms if they exist
+                if ($.fn.ploneTabInit) {
+                    body.ploneTabInit();
                 }
-                setupOverlay();
-                stretch();
-                // Shring iframe when the overlay is closed
+
+                // Tinymce editable areas inside overlay
+                $('textarea.mce_editable', body).each(function() {
+                    var id = $(this).attr('id'),
+                        config = new TinyMCEConfig(id);
+                    // Forgive me for I am about to sin. But it does mean
+                    // we can overlay it multiple times. If you know a
+                    // better way, please share.
+                    delete InitializedTinyMCEInstances[id];
+                    config.init();
+                });
+
+                // Call any other event handlers
+                $(document).trigger('setupOverlay',
+                    [modal, response, error]);
+
+                // Shrink iframe when the overlay is closed
                 modal.on('hidden', function(e){ shrink(); });
+
+                // Show overlay
+                stretch();
                 modal.modal('show');
             }
         );
