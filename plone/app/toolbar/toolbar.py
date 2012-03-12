087ca19890ceed5651f362481fff5ea8d2e1d7e3
Izak Burger <isburger@gmail.com>
2012-03-13 00:43:33 +0200

    overlay modal now in iframe window.

--- plone/app/toolbar/toolbar.py
+++ plone/app/toolbar/toolbar.py
@@ -275,7 +275,15 @@ class Toolbar(ContentViewsViewlet):
                     '  <div class="toolbar-right"><\/div>' +
                     '  <div class="toolbar-left"><\/div>' +
                     ' <\/div>' +
-                    '<\/div>',
+                    '<\/div>' + 
+                    '<div class="modal hide" id="toolbar-overlay">' +
+                    '    <div class="modal-header">' +
+                    '        <a class="close" data-dismiss="modal">&times;</a>' +
+                    '    </div>' +
+                    '    <div class="modal-body">' +
+                    '        <p>One fine body</p>' +
+                    '    </div>' +
+                    '</div>',
                 template_options: function(groups) {
                   return {
                       '.toolbar-right': groups.render_group('personalactions'),
