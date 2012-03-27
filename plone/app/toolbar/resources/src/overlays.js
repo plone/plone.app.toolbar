906925313504b3c585746382e36edba48a0cb8a1
Rok Garbas <rok@garbas.si>
2012-03-27 02:59:18 +0200

    adding license to overlays script and triggering of deco should also happend from here

--- plone/app/toolbar/resources/src/overlays.js
+++ plone/app/toolbar/resources/src/overlays.js
@@ -1,3 +1,39 @@
+//
+// This script is used to provide glue code between iframize and twitter
+// bootstrap modal. And also providing some convinience method for usage in
+// Plone.
+//
+//
+// @author Rok Garbas, Izak Burger
+// @version 0.1
+// @licstart  The following is the entire license notice for the JavaScript
+//            code in this page.
+//
+// Copyright (C) 2010 Plone Foundation
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 2 of the License.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program; if not, write to the Free Software Foundation, Inc., 51
+// Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+// @licend  The above is the entire license notice for the JavaScript code in
+//          this page.
+//
+
+/*jshint bitwise:true, curly:true, eqeqeq:true, immed:true, latedef:true,
+  newcap:true, noarg:true, noempty:true, nonew:true, plusplus:true,
+  regexp:true, undef:true, strict:true, trailing:true, browser:true */
+/*global $:false, jQuery:false */
+
+
 (function($) {
     "use strict";
 
@@ -238,6 +274,19 @@
     // and i probably did, also forgot some.
 
     // ## Edit
+    // # Trigger deco {{{
+    //var iframe = window.parent.$('#' + $.plone.globals.toolbar_iframe_id).iframize('deco-toolbar');
+    //window.parent.$(iframe.el_iframe).bind('iframe_loaded', function() {
+    //    $('#plone-action-edit > a').bind('click', function(e) {
+    //        if ($('[data-iframe="deco-toolbar"]', window.parent.document).size() > 0) {
+    //            e.preventDefault();
+    //            e.stopPropagation();
+    //            $(this).ploneDecoToolbar();
+    //        }
+    //    });
+    //});
+    // }}}
+
     // ## Rules
     // ## Sharing
 
