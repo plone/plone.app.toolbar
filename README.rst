7a6aaa89f9c99754d6001042770f97b1a69897be
Rok Garbas <rok@garbas.si>
2012-05-05 20:12:56 +0200

    split some code out of plone.overlay.js into plone.cmsui.js
    also add documentation how plone.app.toolbar works (README.rst)

--- README.rst
+++ README.rst
@@ -1,3 +1,4 @@
+============
 Introduction
 ============
 
@@ -19,3 +20,57 @@ Plone 4.2a2 - update versions as applicable::
     
 Make sure you install the "Plone Toolbar" profile when creating your
 Plone site.
+
+
+How it works
+============
+
+Registers toolbar tile which creates toolbar html on every page in twitter
+bootstrap structure and is hidden by default. Toolbar tile also lists resouces
+from `toolbar` skin in `data-iframe-resources` attribute of top element. ::
+
+    <div style="display: none;"
+         data-iframe-resouces="resource1.js;resource2.css;...">
+      <div class="navbar">
+        ...
+      </div>
+    </div>
+
+`data-iframe-resouces` attribute is picked out by `iframize.js`_ which creates
+iframe with resources picked from `data-iframe-resources` attribute.
+`iframize.js` script also takes care that dropdown in toolbar which goes
+outside iframe area stretches it in transparent way so that user doesn't even
+notice it.
+
+Same transparent stretching of iframe is used when overlay is opened. This is
+done by `plone.overlay.js`_ script. This script also triggers event when
+toolbar button is clicked. You can subscribe to it you binding to
+`plone_overlay.<id-of-button>` jquery event.::
+
+    // Example of how to bind to Contents button in toolbar
+    $(document).on('plone_overlay.plone-action-folderContents', function(e) {
+        ... do stuff
+    });
+
+Examples of this can be found in `plone.cmsui.js`_ script. At the time of
+writing this documentation `plone.cmsui.js`_ script is not used by
+plone.app.toolbar.
+
+
+More on plone.overlay.js
+========================
+
+TODO: once jquery.form.js is integrated into $.plone.Overlay write
+documentation for it.
+
+
+Todo
+====
+
+ - it should be possible to use custom skin for instead hardcoded `toolbar`
+   skin
+
+
+.. _`iframize.js`: https://github.com/plone/plone.app.toolbar/blob/master/plone/app/toolbar/resources/src/iframize.js
+.. _`plone.overlay.js`: https://github.com/plone/plone.app.toolbar/blob/master/plone/app/toolbar/resources/src/plone.overlay.js
+.. _`plone.cmsui.js`: https://github.com/plone/plone.app.toolbar/blob/master/plone/app/toolbar/resources/src/plone.cmsui.js
---
+++ plone/app/toolbar/resources/src/plone.cmsui.js
@@ -0,0 +1,267 @@
+//
+// This script is used to provide glue code between iframize and twitter
+// bootstrap modal. And also providing some convinience method for usage in
+// Plone.
+//
+//
+// @author Rok Garbas, Izak Burger
+// @version 0.1
+// @licstart  The following is the entire license notice for the JavaScript
+//            code in this page.
+//
+// Copyright (C) 2010 Plone Foundation
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 2 of the License.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program; if not, write to the Free Software Foundation, Inc., 51
+// Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+// @licend  The above is the entire license notice for the JavaScript code in
+//          this page.
+//
+
+/*jshint bitwise:true, curly:true, eqeqeq:true, immed:true, latedef:true,
+  newcap:true, noarg:true, noempty:true, nonew:true, plusplus:true,
+  regexp:true, undef:true, strict:true, trailing:true, browser:true */
+/*global $:false, jQuery:false */
+
+
+(function($) {
+    "use strict";
+
+    // # Common utils {{{
+
+    // ## Forms helper {{{
+    $.plone.overlay_form_transform = function(overlay, data, options) {
+
+        options = $.extend({
+            title_selector: 'h1.documentFirstHeading',
+            content_selector: '#content',
+            buttons_selector: '.formControls > input[type=submit]',
+            cancel_buttons: [
+                "input[name='buttons.cancel']",
+                "input[name='form.button.Cancel']",
+                "input[name='form.button.cancel']",
+                "input[name='form.actions.cancel']"
+                ]
+        }, options || {});
+
+        overlay.title.html('');
+        overlay.title.append($(options.title_selector, data).html());
+
+        // copy content from data into overlay
+        overlay.body.html($(options.content_selector, data).html());
+
+        // set form attributes to form attributes in overlay
+        overlay.form.addClass('form-horizontal');
+        $.each($('form', $(options.content_selector, data))[0].attributes,
+          function(i, attr) {
+            overlay.form.attr(attr.name, attr.value);
+          });
+
+        // trigger tinymce
+        // TODO: for some reason i couldn't get wysiwyg widget to add correct
+        // class for this textarea
+        $('textarea#plone-app-texttile-text', overlay.body).cleditor({
+          controls: "bold italic underline | style removeformat | bullets " +
+                    "numbering | outdent indent | alignleft center " +
+                    "alignright justify | undo redo | link unlink | " +
+                    "pastetext source"
+        });
+
+        // tabs (ala twitter bootstrap)
+        var tabs = $('<ul class="nav nav-tabs"></ul>'),
+            tabs_content = $('<div class="tab-content">'),
+            fieldsets = $('fieldset', overlay.body);
+        fieldsets.each(function(i, fieldset) {
+            fieldset = $(fieldset);
+            tabs.append($('<li/>').append(
+                    $('<a/>').attr('href', '#' + fieldset.attr('id'))
+                        .html($('legend', fieldset).hide().html())
+                        .on('click', function(e) {
+                            e.preventDefault();
+                            $(this).tab('show').blur();
+                        })
+                        ));
+            tabs_content.append($('<div>').addClass('tab-pane')
+                    .attr('id', fieldset.attr('id'))
+                    .html(fieldset.html()));
+            fieldset.remove();
+        });
+        if (fieldsets.size() > 0) {
+            $(overlay.body).prepend(tabs_content).prepend(tabs);
+        }
+
+        $('a', tabs).tab();
+        $('a', tabs).first().tab('show');
+
+        // copy buttons to modal-footer
+        overlay.buttons = $('<div class="pull-right"/>');
+        overlay.footer.html('');
+        overlay.footer.append(overlay.buttons);
+        $(options.buttons_selector, overlay.body).each(function(i, button) {
+            button = $(button);
+            button.addClass('btn');
+            if (button.hasClass('context')) {
+                button.addClass('btn-primary');
+            }
+            overlay.buttons.append(button);
+        });
+
+        // change note should be in footer
+        var change_note = $('#cmfeditions_version_comment_block', overlay.body);
+        if (change_note.size() !== 0) {
+            $('input', change_note)
+                .attr('placeholder', $('.formHelp', change_note).text().trim())
+                .appendTo($('<div class="pull-left"/>')
+                        .appendTo(overlay.footer));
+            change_note.remove();
+        }
+
+        // cancel buttons should close overlay
+         $.each(options.cancel_buttons, function(i, selector) {
+             var button = $(selector, overlay.footer);
+             button.on('click', function(e) {
+                 e.preventDefault();
+                 e.stopPropagation();
+                 overlay.modal('hide');
+             });
+         });
+    };
+    // }}}
+
+    // }}}
+
+    // # Actions {{
+
+    // ## Contents (folder_contents) {{{
+    // FIXME: not working
+    $(document).on('plone_overlay.plone-action-folderContents', function(e) {
+        var self = this;
+
+        $('#folderlisting-main-table a', self.body).each(function(){
+
+            // Remove any parameters from the url
+            var href = $(this).attr('href');
+            var base_href = (href.match(/^([^?]+)/)||[])[1];
+
+            if(base_href.match(/\/folder_contents$/)){
+                var viewlink = $('<a><img src="++resource++plone.app.toolbar/view.png" /></a>')
+                    .attr('href', $(this).attr('href').replace(/\/folder_contents$/, ''))
+                    .attr('class', 'viewlink')
+                    .attr('target', '_parent')
+                    .attr('title', 'Open here'); // Needs i18n!
+                $(this).parent().append(viewlink);
+            } else if (base_href.match(/\/folder_position$/)){
+                // Do nothing, the default click handler already keeps
+                // the result in the overlay
+            } else if (base_href.match(/\/folder_contents$/)) {
+                // It has parameters, leave it alone
+            } else {
+                // Replace click handler
+                $(this).off('click');
+                $(this).on('click', function(e){
+                   window.parent.location.href = $(e.target).attr('href');
+                });
+            }
+        });
+
+        // Add an "Open here" link at the top
+        var viewlink = $('<a><img src="++resource++plone.app.toolbar/view.png" /></a>')
+            .attr('href', e.href.replace(/\/folder_contents$/, ''))
+            .attr('class', 'viewlink')
+            .attr('target', '_parent')
+            .attr('title', 'Open here'); // Needs i18n!
+        $('h1.documentFirstHeading').append(viewlink);
+
+        // Keep forms inside the overlay by placing result of form submission
+        // back into the overlay and calling overlay_setup again.
+        $('form').ajaxForm({
+            success: function (responseText){
+                var modal = $('#toolbar-overlay'),
+                    body = $('.modal-body', modal),
+                    selector = '#portal-column-content > *';
+                // strip inline script tags
+                responseText = responseText.replace(/<script(.|\s)*?\/script>/gi, "");
+                var res = $('<div />').append(responseText)
+                    .find(selector);
+                body.empty().append(res);
+                overlay_setup(modal, body, 'toolbar-button-folderContents', e.href, selector);
+                return false;
+            }
+        });
+
+        // Fix breadcrumbs to go to folder_contents
+        $('#toolbar-overlay #portal-breadcrumbs a').each(function(){
+            $this = $(this);
+            $this.attr('href', $this.attr('href') + '/folder_contents');
+        });
+
+    });
+    // }}}
+
+    // TODO: bellow i listed action which i think we should implement, i might,
+    // and i probably did, also forgot some.
+
+    // ## Edit {{{
+    $(document).on('plone_overlay.plone-action-edit', function(e, overlay) {
+        overlay.load(function(data) {
+
+            $.plone.overlay_form_transform(overlay,
+                $('#portal-columns #portal-column-content', data));
+
+            // hide overlay and trigger deco
+            if ($('[data-iframe="deco-toolbar"]', window.parent.document).size() > 0) {
+              overlay.options = { show: false };
+              var deco_toolbar = $(overlay.el).ploneDecoToolbar();
+              if (deco_toolbar.visible === false) {
+                deco_toolbar.activate();
+              } else {
+                deco_toolbar.deactivate();
+              }
+            }
+        });
+    });
+    // }}}
+
+    // ## Rules
+    // ## Sharing
+
+    // ## Actions -> Cut
+    // ## Actions -> Paste
+    // ## Actions -> Delete
+    // ## Actions -> Rename
+
+    // ## Display -> Select a content item as default view
+
+    // ## Add forms {{{
+    // FIXME: not working
+    $(document).on('plone_overlay.toolbar-button-plone-contentmenu-factories', function(e){
+        // Submit form using ajax, then close modal and reload parent
+        var modal = $('#toolbar-overlay', toolbar.document),
+            body = $('.modal-body', modal);
+        $('form', body).ajaxForm({
+            success: function() {
+                modal.modal('hide');
+                body.empty();
+                window.parent.location.replace(window.parent.location.href);
+            }
+        });
+    });
+    // }}}
+
+    // ## State: ??? -> Publish, Submit for publication, Retract, Send back
+    // ## State: ??? -> Advanced...
+
+    // ## Personal -> Preferences
+    // ## Personal -> Site Setup
+
+}(jQuery));
