bd4eb8b2ced68959812773aa773d9bfb7c3e8575
Rok Garbas <rok@garbas.si>
2012-05-27 13:32:53 +0200

    Revert "Revert "added copyright notice, comments to code and provide some same naming.""
    This reverts commit 44722b1d8c3f9571ca5d3e83835b1a96abbe2e74.

--- plone/app/toolbar/resources/src/plone.toolbar.js
+++ plone/app/toolbar/resources/src/plone.toolbar.js
@@ -1,66 +1,114 @@
+// This plugin is used to handle all clicks inside toolbar iframe.
+//
+// @author Rok Garbas
+// @version 1.0
+// @licstart  The following is the entire license notice for the JavaScript
+//            code in this page.
+//
+// Copyright (C) 2010 Plone Foundation
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 2 of the License.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program; if not, write to the Free Software Foundation, Inc., 51
+// Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+// @licend  The above is the entire license notice for the JavaScript code in
+//          this page.
+//
+
+/*jshint bitwise:true, curly:true, eqeqeq:true, immed:true, latedef:true,
+  newcap:true, noarg:true, noempty:true, nonew:true, plusplus:true,
+  regexp:true, undef:true, strict:true, trailing:true, browser:true */
+/*global jQuery:false */
+
+
 (function($) {
+"use strict";
 
-$.iframe = $.iframe || {};
+// # Namespace
+//
+// ensure proper namespaces
+$.plone = $.plone || {};
+$.plone.toolbar = $.plone.toolbar || {};
 
-$.iframe.obj = window.parent.iframed[window.name],
-    $.iframe.iframe_state;
+// # IFrame Object
+//
+// nicer way to create shortcut 
+$.plone.toolbar.iframe = window.parent.iframed[window.name];
 
-$.iframe.shrink = function() {
-  if ($.iframe.iframe_state !== undefined) {
-    $($.iframe.obj.el).height($.iframe.iframe_state.height);
-    $($.iframe.obj.el).offset($.iframe.iframe_state.offset);
-    $.iframe.iframe_state = undefined;
+// # Shrink IFrame Object
+//
+// Shrink current frame to the size that was before stretching it.
+$.plone.toolbar.iframe_shrink = function() {
+  if ($.plone.toolbar.iframe_state !== undefined) {
+    $($.plone.toolbar.iframe.el).height($.plone.toolbar.iframe_state.height);
+    $($.plone.toolbar.iframe.el).offset($.plone.toolbar.iframe_state.offset);
+    $.plone.toolbar.iframe_state = undefined;
   }
-}
+};
 
-// stretch over whole parent document
-$.iframe.stretch = function() {
-  if ($.iframe.iframe_state === undefined) {
-    $.iframe.iframe_state = {};
-    $.iframe.iframe_state.height = $($.iframe.obj.el).height();
-    $.iframe.iframe_state.offset = $($.iframe.obj.el).offset();
-    $($.iframe.obj.el).height($(window.parent.document).height());
-    $($.iframe.obj.el).offset({top: 0, left: 0});
+// # Stretch IFrame Object
+//
+// This function stretches current frame over whole top frame while keeping
+// iframe object trasparent
+$.plone.toolbar.iframe_stretch = function() {
+  if ($.plone.toolbar.iframe_state === undefined) {
+    $.plone.toolbar.iframe_state = {};
+    $.plone.toolbar.iframe_state.height = $($.plone.toolbar.iframe.el).height();
+    $.plone.toolbar.iframe_state.offset = $($.plone.toolbar.iframe.el).offset();
+    $($.plone.toolbar.iframe.el).height($(window.parent.document).height());
+    $($.plone.toolbar.iframe.el).offset({top: 0, left: 0});
   }
-}
+};
 
-$(document).on('click', function(e) { $.iframe.shrink(); });
+// # Shrink on any non binded click happends
+$(document).on('click', function(e) {
+  $.plone.toolbar.iframe_shrink();
+});
 
-$('body > .navbar a').on('click', function(e) {
+// # Handle every click on every link inside current frame 
+// 
+// Currently script recognizes:
+//  - twitter bootstrap dropdown
+//  - ploneOverlay links
+$('a').on('click', function(e) {
+
+  // only handle left(1) and right(2) click
   if (e.which === 1 || e.which === 2) {
-    var el = $(this),
-        id = el.attr('id'),
-        event_exists = false;
+    var el = $(this);
+
+    // since we'll be handling this link we prevent any default behaivour
     e.preventDefault();
 
-    if (id === undefined) {
-      id = el.parents('[id]').attr('id');
-    }
-
-    if ($(document).data('events') !== undefined) {
-      $.each($(document).data('events').plone_toolbar || [], function(i, e) {
-        if (e.namespace === id) {
-          event_exists = true;
-          return;
-        }
-      });
-    }
-
+    // if link has twitter bootstrap dropdown assigned to it then make sure 
+    // current frame gets streched invisibly just in case dropdown goes over
+    // the area of top frame.
     if (el.attr('data-toggle') === 'dropdown') {
-        $.iframe.stretch();
+      $.plone.toolbar.iframe_stretch();
 
-    // Buttons default to an overlay but if they
-    // have the '_parent' link target, just load them in
-    // the top window
-    } else if (el.attr('target') === '_parent' || event_exists === false) {
+    // if ploneOverlay is registered for currently clicked link then we make
+    // sure that iframe is stretched before being open and shrank on closing
+    } else if (el.data('plone-overlay') !== undefined) {
+      el.data('plone-overlay').el
+        .on('show', function(e) { $.plone.toolbar.iframe_stretch(); })
+        .on('hide', function(e) { $.plone.toolbar.iframe_shrink(); });
+
+    // if none of above conditions is met then open link in top frame or new
+    // window in case right button was used
+    } else {
       if (e.which === 1) {
         window.parent.location.href = el.attr('href');
       } else {
         window.parent.open(el.attr('href'));
       }
-
-    } else {
-      $(document).trigger('plone_toolbar.' + id, el);
     }
   }
 });
