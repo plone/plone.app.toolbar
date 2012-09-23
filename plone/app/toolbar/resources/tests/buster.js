472a4f226df09091a5d1a67c4796a97ad18105d9
Ross Patterson <me@rpatterson.net>
2012-09-23 16:21:23 -0700

    Add Buster.JS lint and coverage

--- plone/app/toolbar/resources/tests/buster.js
+++ plone/app/toolbar/resources/tests/buster.js
@@ -1,5 +1,40 @@
 var config = module.exports;
 
+var linterConf = {
+    linter: 'jshint',
+        linterOptions: {
+            asi: false,
+            bitwise: true,
+            boss: false,
+            browser: true,
+            curly: true,
+            devel: false,
+            eqeqeq: true,
+            evil: false,
+            expr: false,
+            forin: false,
+            immed: true,
+            jquery: true,
+            latedef: false,
+            mootools: false,
+            newcap: true,
+            node: false,
+            noempty: true,
+            nomen: false,
+            nonew: true,
+            onevar: false,
+            plusplus: false,
+            regexp: true,
+            strict: false,
+            supernew: true,
+            undef: true,
+            white: false
+        },
+        excludes: [
+            "jquery"
+       ]
+};
+
 config["My tests"] = {
     rootPath: "../",
     environment: "browser",
@@ -8,5 +43,14 @@ config["My tests"] = {
     ],
     tests: [
         "tests/*-test.js"
-    ]
+    ],
+    extensions: [
+        require('buster-lint'),
+        require("buster-coverage")
+    ],
+    "buster-lint": linterConf,
+    "buster-coverage": {
+        format: "cobertura",
+        combinedResultsOnly: true
+    }
 };
