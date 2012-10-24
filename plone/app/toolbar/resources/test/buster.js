727601f7533117dd7a7dbf79592349948755462c
Rok Garbas <rok@garbas.si>
2012-10-24 13:51:05 +0200

    running tests against jquery 1.7 and 1.8

--- plone/app/toolbar/resources/test/buster.js
+++ plone/app/toolbar/resources/test/buster.js
@@ -1,33 +1,42 @@
-var config = module.exports;
+var _ = require('underscore'),
+    config = module.exports,
+    default_config = {
+      rootPath: "../",
+      environment: "browser",
+      testHelpers: [
+        'test/helpers.js'
+      ],
+      resources: [
+        'test/example-resource.css',
+        'test/example-resource.js'
+      ],
+      sources: [
+        'src/iframe.js',
+        'src/jquery.iframe.js',
+        'src/plone.init.js',
+        'src/plone.mask.js',
+        'src/plone.overlay.js',
+        'src/plone.cmsui.js',
+        'src/plone.tabs.js'
+      ],
+      tests: [
+        'test/iframe-test.js',
+        'test/jquery.iframe-test.js'
+      ],
+      extensions: [
+        require("buster-coverage")
+      ],
+      "buster-coverage": { combinedResultsOnly: true }
+      };
 
 
-config["plone.app.toolbar"] = {
-    rootPath: "../",
-    environment: "browser",
-    testHelpers: [
-      'test/helpers.js'
-    ],
-    resources: [
-      'test/example-resource.css',
-      'test/example-resource.js'
-    ],
-    libs: [
-      'lib/jquery-1.7.2.min.js'
-    ],
-    sources: [
-      'src/iframe.js',
-      'src/jquery.iframe.js',
-      'src/plone.init.js',
-      'src/plone.mask.js',
-      'src/plone.overlay.js',
-      'src/plone.cmsui.js',
-      'src/plone.tabs.js'
-    ],
-    tests: [
-      'test/iframe-test.js',
-    ],
-    extensions: [
-      require("buster-coverage")
-    ],
-    "buster-coverage": { combinedResultsOnly: true }
-};
+config["plone.app.toolbar:jquery-1.7"] = _.extend({}, default_config, {
+  libs: [
+    'lib/jquery-1.7.2.min.js'
+  ]
+});
+config["plone.app.toolbar:jquery-1.8"] = _.extend({}, default_config, {
+  libs: [
+    'lib/jquery-1.8.2.min.js'
+  ]
+});
