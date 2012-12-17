eceef0d225f5a17d861b1c92ae97bd1006c865d2
Rok Garbas <rok@garbas.si>
2012-12-17 19:45:30 +0100

    adding p.a.widgets as dependency (we'll move all non toolbar related code there)

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(
         'plone.tiles',
         'Acquisition',
         'plone.app.jquery>=1.8.3',
+        'plone.app.widgets',
         'Products.ResourceRegistries>=2.1',
 #        'Products.CMFPlone>4.3a2',
 #        'Products.TinyMCE>1.3b7',
