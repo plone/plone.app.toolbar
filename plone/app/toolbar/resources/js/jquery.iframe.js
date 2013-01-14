a8e709e35192167cfe5b4079f3835b77059ec9ce
Rok Garbas <rok@garbas.si>
2013-01-14 18:55:16 +0100

    remove iframe "actions" and trigger events on iframe, also act uppon resize event on iframe

--- plone/app/toolbar/resources/js/jquery.iframe.js
+++ plone/app/toolbar/resources/js/jquery.iframe.js
@@ -44,65 +44,49 @@ $.IFrame.prototype = {
     self.$el = $(iframe.el);
     self.window = window.parent;
     self.document = window.parent.document;
-    self._state = null;
-    self._actions = [];
+    self.is_stretched = false;
 
     // # Handle clicks inside iframe
     $(document).on('click', function(e) {
-      var condition = false;
 
-      // see if there is any link action registered to handle this click
-      $.each(self._actions, function(i, item) {
-        if (item[0].apply(this, [e, self])) {
-          item[1].apply(this, [e, self]);
-        }
-      });
+      // opens link in top frame if clicked on it
+      // TODO: only if on same base url
+      if ($.nodeName(e.target, 'a') ||
+          $(e.target).parents('a').size() === 1) {
 
-    });
-
-    // register common actions
-
-    // which opens link in top frame if clicked on
-    self.registerAction(
-      function(e) {
-        return ($.nodeName(e.target, 'a') ||
-                $(e.target).parents('a').size() === 1);
-      },
-      function(e) {
         e.stopPropagation();
         e.preventDefault();
+
         var url = $(e.target).attr('href');
         if (!$.nodeName(e.target, 'a')) {
           url = $(e.target).parents('a').attr('href');
         }
+
         if (e.which === 1) {
-          self._window_location(url);
+          self._same_window(url);
         } else if (e.which === 2) {
-          self._window_open(url);
+          self._new_window(url);
         }
-      });
 
-    // shrink iframe when click happens on stretched & transparent part of iframe
-    self.registerAction(
-        function(e) { return $.nodeName(e.target, 'html'); },
-        function(e) { self.shrink(); });
+      // if click on any other element then 'a' of iframe shrink it
+      //return $.nodeName(e.target, 'html');
+      } else {
+        self.shrink();
+      }
 
-    $(self.window).on('resize', function(e) {
-      self.fit();
     });
+
+    // make sure all content of iframe is visible
+    self.fit();
+    self.$el.on('resize', function(e) { self.fit(); });
   },
 
   // Abstract calls to window.parent so its easier to stub/mock in tests
-  _window_location: function(url) {
+  _same_window: function(url) {
     this.window.location.href = url;
   },
-
-  _window_open: function(url) {
+  _new_window: function(url) {
     this.window.open(url);
-  },
-
-  registerAction: function(condition, action) {
-    this._actions.push([ condition, action ]);
   },
 
   // # Shrink IFrame Object
@@ -110,9 +94,11 @@ $.IFrame.prototype = {
   // Shrink current frame to the size that was before stretching it.
   shrink: function() {
     var self = this;
-    if (self._state !== null) {
-      self.$el.css(self._state);
-      self._state = null;
+    if (self.is_stretched) {
+      self.$el.trigger('iframe.shrink');
+      self.is_stretched = false;
+      self.fit();
+      self.$el.trigger('iframe.shrinked');
     }
   },
 
@@ -122,19 +108,11 @@ $.IFrame.prototype = {
   // iframe object trasparent
   stretch: function() {
     var self = this;
-    if (self._state === null) {
-      var offset = self.$el.offset();
-
-      self._state = {};
-      self._state.top = offset.top;
-      self._state.left = offset.left;
-      self._state.height = self.$el.height();
-
-      self.$el.css({
-        top: 0,
-        left: 0,
-        height: $(self.document).height()
-      });
+    if (!self.is_stretched) {
+      self.$el.trigger('iframe.stretch');
+      self.is_stretched = true;
+      self.$el.css({ height: $(self.document).height() });
+      self.$el.trigger('iframe.stretched');
     }
   },
 
@@ -144,21 +122,18 @@ $.IFrame.prototype = {
   // action (stretch or shrink)
   toggle: function() {
     var self = this;
-    if (self._state === null) {
-      self.stretch();
-    } else {
-      self.shrink();
-    }
+    if (!self.is_stretched) { self.stretch(); }
+    else { self.shrink(); }
   },
 
   fit: function() {
     var self = this;
-    if (self._state !== null) {
-      self._state.height = $(document).height();
+    if (!self.is_stretched) {
+      self.$el.css({ height: $('body', document).height() });
+    } else {
+      self.$el.css({ height: $(self.document).height() });
     }
-    self.$el.css('height', $(document).height());
   }
-
 };
 
 if (window.parent.iframe !== undefined && window.name &&
