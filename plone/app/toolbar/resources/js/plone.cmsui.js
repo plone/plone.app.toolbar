0cfc065e3efa807852b4d811c8cc91ea421b6b7a
Rok Garbas <rok@garbas.si>
2013-01-15 10:58:23 +0100

    enabling remove and delete actions

--- plone/app/toolbar/resources/js/plone.cmsui.js
+++ plone/app/toolbar/resources/js/plone.cmsui.js
@@ -32,47 +32,6 @@
 
 $(document).ready(function() {
 
-  //// Delete Action
-  //$('#plone-toolbar #plone-contentmenu-actions > ul > li#delete > a').ploneOverlay({
-  //  onShow: function() { $(this).patternToggle('toggle'); },
-  //    formButtons: {
-  //      '.modal-body input[name="form.button.Cancel"]':
-  //          $.fn.ploneOverlay.defaultAjaxSubmit(),
-  //      '.modal-body input.destructive':
-  //          $.fn.ploneOverlay.defaultAjaxSubmit({
-  //            onSave: function(response, state, xhr, form, button) {
-  //              // need redirect to different url after successfull submitting
-  //              window.parent.location.href = this.getBaseURL(xhr.responseText);
-  //            }
-  //          })
-  //    }
-  //});
-
-  //// Rename Action
-  //$('#plone-toolbar #plone-contentmenu-actions > ul > li#rename > a').ploneOverlay({
-  //  onShow: function() { $(this).patternToggle('toggle'); },
-  //  formButtons: {
-  //    '.modal-body input[name="form.button.Cancel"]':
-  //        $.fn.ploneOverlay.defaultAjaxSubmit(),
-  //    '.modal-body input[name="form.button.RenameAll"]':
-  //        $.fn.ploneOverlay.defaultAjaxSubmit({
-  //          onSave: function(response, state, xhr, form, button) {
-  //            // need redirect to different url after successfull submitting
-  //            window.parent.location.href = this.getBaseURL(xhr.responseText);
-  //          }
-  //        })
-  //  }
-  //});
-
-  //// Display View -> Select default content as default view
-  //$('#plone-toolbar #plone-contentmenu-display > ul > li#contextSetDefaultPage > a').ploneOverlay({
-  //  onShow: function() { $(this).patternToggle('toggle'); },
-  //  formButtons: {
-  //    '.modal-body input[name="form.button.Cancel"]': $.fn.ploneOverlay.defaultAjaxSubmit(),
-  //    '.modal-body input[name="form.button.Save"]': $.fn.ploneOverlay.defaultAjaxSubmit()
-  //  }
-  //});
-
   // Edit form
   $('#plone-toolbar #plone-action-edit > a').ploneOverlay({
     events: {
@@ -117,6 +76,29 @@ $(document).ready(function() {
         contentFilters: []
       }
     }
+  });
+
+  // Delete Action
+  $('#plone-toolbar #plone-contentmenu-actions > ul > li#plone-contentmenu-actions-delete > a').ploneOverlay({
+    events: {
+      'click .modal-body input[name="form.button.Cancel"]': {},
+      'click .modal-body input.destructive': {}
+    }
+  });
+
+  // Rename Action
+  $('#plone-toolbar #plone-contentmenu-actions > ul > li#plone-contentmenu-actions-rename > a').ploneOverlay({
+    events: {
+      'click .modal-body input[name="form.button.Cancel"]': {},
+      'click .modal-body input[name="form.button.RenameAll"]': {}
+    }
+    //'.modal-body input[name="form.button.RenameAll"]':
+    //    $.fn.ploneOverlay.defaultAjaxSubmit({
+    //      onSave: function(response, state, xhr, form, button) {
+    //        // need redirect to different url after successfull submitting
+    //        window.parent.location.href = this.getBaseURL(xhr.responseText);
+    //      }
+    //    })
   });
 
   // Change content item as default view...
