aad515f140bc61ca0d88daf64b8d6829d6978ad8
Rok Garbas <rok@garbas.si>
2013-07-15 17:10:56 +0200

    updating toolbar.js to work with new bower setup

--- plone/app/toolbar/static/toolbar.js
+++ plone/app/toolbar/static/toolbar.js
@@ -1,21 +1,25 @@
-
 (function($) {
   "use strict";
 
   $(document).ready(function() {
 
-    var script = document.createElement('script');
-    script.setAttribute('type', 'text/javascript');
-    script.setAttribute('src', '++resource++mockup/jam/require.js');
-    script.onload = function() {
-      requirejs.config({ baseUrl: '++resource++mockup/' });
-      window.require(['js/bundles/toolbar'], function(Widgets) {
-        Widgets.scan('body');
-      });
+    var script1 = document.createElement('script');
+    script1.setAttribute('type', 'text/javascript');
+    script1.setAttribute('src', '/++resource++mockup/bower_components/requirejs/require.js');
+    script1.onload = function() {
+      var script2 = document.createElement('script');
+      script2.setAttribute('type', 'text/javascript');
+      script2.setAttribute('src', '/++resource++mockup/js/config.js');
+      script1.onload = function() {
+        requirejs.config({ baseUrl: '++resource++mockup/' });
+        window.require(['mockup-bundles-toolbar'], function(Toolbar) {
+          Toolbar.scan('body');
+        });
+      };
+      document.getElementsByTagName("head")[0].appendChild(script2);
     };
-    document.getElementsByTagName("head")[0].appendChild(script);
+    document.getElementsByTagName("head")[0].appendChild(script1);
 
   });
 
 }(jQuery));
-
